3cd533aa30266dc806c604faf49a0082ab8741ce
Sergey Prishchepa <adamantine007@gmail.com>
2015-05-17 13:45:14 +0300

    Add icon for admin menu

--- app/Admin/menu.php
+++ app/Admin/menu.php
@@ -5,31 +5,31 @@ use SleepingOwl\Admin\Admin;
 //Admin::menu()->url('/')->label('Start page')->icon('fa-dashboard');
 
 
-Admin::menu()->label('Shop')->items(function()
+Admin::menu()->label('Shop')->icon('fa-shopping-cart')->items(function()
 {
-    Admin::menu('App\Models\Order');
-    Admin::menu('App\Models\ExecutedOrder');
-    Admin::menu('App\Models\OrderStatus');
+    Admin::menu('App\Models\Order')->icon('fa-list');
+    Admin::menu('App\Models\ExecutedOrder')->icon('fa-check-square-o');
+    Admin::menu('App\Models\OrderStatus')->icon('fa-asterisk');
 });
 
-Admin::menu()->label('Products')->items(function()
+Admin::menu()->label('Products')->icon('fa-thumbs-o-up')->items(function()
 {
-    Admin::menu('App\Models\Product');
-    Admin::menu('App\Models\ProductInfo');
-    Admin::menu('App\Models\Producer');
+    Admin::menu('App\Models\Product')->icon('fa-thumbs-o-up');
+    Admin::menu('App\Models\ProductInfo')->icon('fa-info-circle');
+    Admin::menu('App\Models\Producer')->icon('fa-sitemap');
 });
 
-Admin::menu()->label('Supplies')->items(function()
+Admin::menu()->label('Supplies')->icon('fa-truck')->items(function()
 {
-    Admin::menu('App\Models\DeliveryOrder');
-    Admin::menu('App\Models\Supply');
-    Admin::menu('App\Models\Supplier');
-    Admin::menu('App\Models\SupplyStatus');
+    Admin::menu('App\Models\DeliveryOrder')->icon('fa-list');
+    Admin::menu('App\Models\Supply')->icon('fa-check-square-o');
+    Admin::menu('App\Models\Supplier')->icon('fa-list-ol');
+    Admin::menu('App\Models\SupplyStatus')->icon('fa-asterisk');
 });
 
-Admin::menu()->label('Backend')->items(function()
+Admin::menu()->label('Backend')->icon('fa-cogs')->items(function()
 {
-    Admin::menu('App\Models\Salary');
+    Admin::menu('App\Models\Salary')->icon('fa-money');
 });
 
 Admin::menu('App\Models\User')->icon('fa-users');
